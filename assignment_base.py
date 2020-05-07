b5c9292e2116bd8de0137e1dd3defa5e2ff8b72f
huqi <huqi@zhuojianchina.com>
2020-05-07 16:10:05 +0800

    check_words method first edition

--- assignment_base.py
+++ assignment_base.py
@@ -2,9 +2,26 @@ import nltk
 nltk.data.path.append("/Users/walking/Project/pythonProject/nltk_data")
 from nltk import ne_chunk, pos_tag, word_tokenize
 from nltk.tree import Tree
+import json
+from nltk.corpus import wordnet as wn
 
 file_url = "./ABCNewsArticle/Are we using ventilators too much?.txt"
 
+def check_words(dict, word):
+    # the word in wordnet will concat with '_' in every single word
+    true_word = str(word).replace(" ", "_")
+    # Check synonyms for each word
+    for word_exist in dict:
+        for same_words_set in wn.synsets(word_exist):
+            for word in same_words_set.lemma_names():
+                if true_word == word:
+                    # which means they are the same words
+                    dict[word_exist] += 1
+                    return dict
+    dict[word] = 1
+    return dict
+
+# method in nltk_trucks.py, which used to extract the entities in the article
 def get_continuous_chunks(text):
     # byte = text.encode()
     chunked = ne_chunk(pos_tag(word_tokenize(text)))
@@ -37,13 +54,32 @@ try:
     temp = story.replace('\\n', '').replace('[', '').replace(']', '')\
         .replace("'", '').replace('"', '').replace("None,", '')
     # end format
-    # An optional qualifier (DT),followed by several adjectives(JJ),
-    # and then followed by a noun (NN), should form a noun phrase(NP)
+    org_dict = {}
+    per_dict = {}
+    gpe_dict = {}
+    data = {}
+    data["ORGANIZATION"] = org_dict
+    data["GPE"] = gpe_dict
+    data["PERSON"] = per_dict
+    times = 1
+    entity_sets = get_continuous_chunks(temp)
     print(get_continuous_chunks(temp))
     for sent in nltk.sent_tokenize(temp):
         for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
             if hasattr(chunk, 'label'):
-                print(chunk.label(), ' '.join(c[0] for c in chunk))
+                word = ' '.join(c[0] for c in chunk)
+                word_type = chunk.label()
+                if word_type == "ORGANIZATION":
+                    org_dict = check_words(org_dict, word)
+                if word_type == "GPE":
+                    gpe_dict = check_words(gpe_dict, word)
+                if word_type == "PERSON":
+                    # person name should not be checked
+                    per_dict.update({word: times})
+                # print(chunk.label(), ' '.join(c[0] for c in chunk))
+
+    jsonStr = json.dumps(data)
+    print(jsonStr)
 
 finally:
     if f:
--- nltk_wn_test.py
+++ nltk_wn_test.py
@@ -5,5 +5,28 @@ from nltk.corpus import wordnet as wn
 word_sets = wn.synsets('china')
 print('similar words set:', word_sets)
 print('similar words set contains:', [set.lemma_names() for set in word_sets])
+# output of contains: [['China', "People's_Republic_of_China", 'mainland_China',
+# 'Communist_China', 'Red_China', 'PRC', 'Cathay'], ['china'], ['Taiwan', 'China',
+# 'Nationalist_China', 'Republic_of_China'], ['chinaware', 'china']]
 print('similar words set details:', [set.definition() for set in word_sets])
 print('similar words set examples:', [set.examples() for set in word_sets])
+
+def check_words(dict, word):
+    # the word in wordnet will concat with '_' in every single word
+    true_word = str(word).replace(" ", "_")
+    # Check synonyms for each word
+    for word_exist in dict:
+        for same_words_set in wn.synsets(word_exist):
+            for word in same_words_set.lemma_names():
+                if true_word == word:
+                    # which means they are the same words
+                    dict[word_exist] += 1
+                    return dict
+    dict[word] = 1
+    return dict
+
+test_dict = {}
+test_dict = check_words(test_dict, "china")
+print("1111", test_dict)
+test_dict = check_words(test_dict, "Nationalist China")
+print("2222", test_dict)
